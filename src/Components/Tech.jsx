2fdee3a7fcdea89d130f97ecb806d9d00cba633a
devalto <devalto@gmail.com>
2023-12-07 13:47:18 -0800

    fixed positioning classes and parent structure

--- src/Components/Tech.jsx
+++ src/Components/Tech.jsx
@@ -3,51 +3,43 @@ import React from 'react'
 import { FaJava } from 'react-icons/fa'
 import { SiJavascript, SiHtml5, SiCss3, SiTypescript, SiPython, SiLua, SiRust, SiCplusplus } from 'react-icons/si'
 
+import conveyorTreading from '../assets/conveyor_treading.png'
 import CoderCat from '../assets/CoderCat.png'
 
-const TECHNOLOGY_CLASSSET = 'title-main my-auto mx-10 font-semibold text-2xl'
+const TECH_TEXT_CLASSES = 'title-main my-auto mx-10 font-semibold text-2xl'
+const TECH_ICON_CLASSES = 'my-auto w-[4vw] h-full'
 
 const TECHNOLOGIES = {
-    'JavaScript': (<SiJavascript size={'100%'} color='rgb(255, 220, 50)'/>),
-    'HTML': (<SiHtml5 size={'100%'} color='rgb(255, 80, 50)'/>),
-    'CSS': (<SiCss3 size={'100%'} color='rgb(120, 120, 255)'/>),
-    'TypeScript': (<SiTypescript size={'100%'} color='rgb(80, 120, 255)'/>),
-    'Python': (<SiPython size={'100%'} color='rgb(255, 230, 100)'/>),
-    'Lua': (<SiLua size={'100%'} color='rgb(80, 80, 255)'/>),
-    'Rust': (<SiRust size={'100%'} color='rgb(255, 150, 100)'/>),
-    'C++': (<SiCplusplus size={'100%'} color='rgb(50, 150, 240)'/>),
-    'Java': (<FaJava size={'100%'} color='rgb(255, 100, 100)'/>),
+    'JavaScript': (<SiJavascript color='rgb(255, 220, 50)' className={TECH_ICON_CLASSES}/>),
+    'HTML': (<SiHtml5 color='rgb(255, 80, 50)' className={TECH_ICON_CLASSES}/>),
+    'CSS': (<SiCss3 color='rgb(120, 120, 255)' className={TECH_ICON_CLASSES}/>),
+    'TypeScript': (<SiTypescript color='#3178C6' className={TECH_ICON_CLASSES}/>),
+    'Python': (<SiPython color='rgb(255, 230, 100)' className={TECH_ICON_CLASSES}/>),
+    'Lua': (<SiLua color='rgb(80, 80, 255)' className={TECH_ICON_CLASSES}/>),
+    'Rust': (<SiRust color='rgb(255, 150, 100)' className={TECH_ICON_CLASSES}/>),
+    'C++': (<SiCplusplus color='rgb(50, 150, 240)' className={TECH_ICON_CLASSES}/>),
+    'Java': (<FaJava color='rgb(255, 100, 100)' className={TECH_ICON_CLASSES}/>),
 }
 
 const Tech = () => {
   return (
     <div name='tech' className='w-full h-[40vh] bg-[#0a192f] text-gray-300 overflow-hidden'>
         <h1 name='title' className='title-main text-2xl text-center sm:text-4xl text-shadow shadow-black font-bold text-[#e7e9ef]'>Explore Technologies</h1>
+ 
+        
 
-        <div className='my-10 w-[500vw] overflow-hidden  '>
-            <motion.div className='flex' animate={{transform: `translate(100vw)`}} initial={{transform: 'translate(-160vw)'}} transition={{repeat: Infinity, ease: 'linear', duration: 25}}>
+        <div className='flex my-10 h-[20vh] bg-contain bg-center' style={{backgroundImage: `url(${conveyorTreading})`}}>
+            <motion.div className='flex h-full' initial={{transform: 'translateX(-100%)'}} animate={{transform: `translateX(100%)`}} transition={{repeat: Infinity, ease: 'linear', duration: 25}}>
                 {Object.entries(TECHNOLOGIES).map(([technology_name]) => {
-                    const className = `flex h-[10vh] mx-5`
+                    const className = `flex h-full mx-5`
                     return (
                         <div key={technology_name} className={className}>
                             {TECHNOLOGIES[technology_name]}
-                            <h1 className={TECHNOLOGY_CLASSSET}>{technology_name}</h1>
+                            <h1 className={TECH_TEXT_CLASSES}>{technology_name}</h1>
                         </div>
                     )
                 })}
             </motion.div>
-            <motion.div className='flex my-8' animate={{transform: `translate(-160vw)`}} initial={{transform: 'translate(100vw)'}} transition={{repeat: Infinity, ease: 'linear', duration: 25, delay: 5}}>
-                {Object.entries(TECHNOLOGIES).map(([technology_name]) => {
-                    const className = `flex h-[10vh] mx-5`
-                    return (
-                        <div key={technology_name} className={className}>
-                            {TECHNOLOGIES[technology_name]}
-                            <h1 className={TECHNOLOGY_CLASSSET}>{technology_name}</h1>
-                        </div>
-                    )
-                })}
-            </motion.div>
-             
         </div>
     </div>
   )
