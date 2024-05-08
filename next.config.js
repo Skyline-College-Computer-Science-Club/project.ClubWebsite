2ac8bbdf557e47df66504e3fcbf43ebb05166152
devalto <51037255+dev-alto@users.noreply.github.com>
2024-05-07 20:37:03 -0700

    ➕ Reintroduced "Prettier" code formatter
    These setup changes follow initalization steps highlighted in #8.

--- next.config.js
+++ next.config.js
@@ -4,12 +4,13 @@ const nextConfig = {
         return [
             {
                 source: '/p5js',
-                destination: 'https://www.canva.com/design/DAGCbZ2TaH4/ZSVhmMFmn8pSIa-GyE5nBQ/view?utm_content=DAGCbZ2TaH4&utm_campaign=designshare&utm_medium=link&utm_source=editor',
+                destination:
+                    'https://www.canva.com/design/DAGCbZ2TaH4/ZSVhmMFmn8pSIa-GyE5nBQ/view?utm_content=DAGCbZ2TaH4&utm_campaign=designshare&utm_medium=link&utm_source=editor',
                 permanent: false,
-                basePath: false
+                basePath: false,
             },
         ]
     },
-};
+}
 
 module.exports = nextConfig
